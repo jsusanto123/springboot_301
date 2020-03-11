c692f82d1ee409aa03056b2ad742b2fe77a495f5
jsusanto123 <jsus8888@gmail.COM>
2020-03-11 19:52:33 -0400

    Initial commit

---
+++ src/test/java/com/julie/springboot_301/Springboot301ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julie.springboot_301;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot301ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
